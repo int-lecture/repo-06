550054cfb54aa37987e100b0f651539265537bf1
1521092Dennis <1521092@stud.hs-mannheim.de>
2017-05-15 11:51:59 +0200

    Login Aktualisiert

--- LoginServer/src/loginServer/Login.java
+++ LoginServer/src/loginServer/Login.java
@@ -26,8 +26,19 @@ public class Login {
 	public static final String ISO8601 = "yyyy-MM-dd'T'HH:mm:ssZ";
 
 	private static Map<String, String> passwordList = new HashMap<>();
-	private static ArrayList<String> tokenList = new ArrayList();
+	private static Map<String, String> tokenList = new HashMap<>();
 
+	/*
+	 * Verbesserung: DB und SQL statt password liste.
+	 * Wie kann man das Expire-Date selber setzen, mit vorgefertigten Funktionen.
+	 * Token mit JWT oder Random erstellen.
+	 * Email auf gültigkeit Testen.
+	 * 
+	 * RestAssured zum Testen von RestServices
+	 */
+	
+	
+	
 	@POST
 	@Path("/login")
 	@Produces(MediaType.APPLICATION_JSON)
@@ -40,15 +51,23 @@ public class Login {
 				|| !jsonObject.optString("pseudonym").isEmpty()) {
 			// Wenn das Passwort falsch ist dann sende Unautorisiert ohne
 			// Message.
-			String user = jsonObject.optString("name");
-			String password = jsonObject.optString("password");
-			if (checkPassword(user, password)) {
-				JSONObject tokenObject = new JSONObject();
-				tokenObject.put("token", createToken("testToken")).put("expire-date", new Date());
-				return Response.ok().entity(tokenObject.toString()).build();
-
+			if(passwordList.containsKey(jsonObject.optString("pseudonym"))){
+				String password = jsonObject.optString("password");
+				String pseudonym = jsonObject.optString("pseudonym");
+				if (checkPassword(pseudonym, password)) {
+					JSONObject tokenObject = new JSONObject();
+					//SimpleDateFormat statt Date();
+					//Token mit JWT erstellen oder über eine Liste von vorgefertigten Tokens.
+					tokenObject.put("token", createToken(pseudonym,"testtoken")).put("expire-date", new Date(new Date().getTime()+5*60*1000));
+					return Response.ok().entity(tokenObject.toString()).build();
+				}else{
+					return Response.status(Response.Status.UNAUTHORIZED).build();
+					
+				}
+			
 			} else {
-				return Response.status(Response.Status.UNAUTHORIZED).build();
+				passwordList.put(jsonObject.optString("pseudonym"), createToken(jsonObject.optString("pseudonym"),jsonObject.optString("password")));
+				return Response.ok().status(Response.Status.CREATED).build();
 			}
 		} else {
 			return Response.status(Response.Status.BAD_REQUEST).build();
@@ -61,9 +80,11 @@ public class Login {
 	@Consumes(MediaType.APPLICATION_JSON)
 	public Response auth(String jsonString) throws JSONException {
 		JSONObject jsonObject = new JSONObject(jsonString);
-		if (!jsonObject.optString("token").isEmpty() || !jsonObject.optString("expire-date").isEmpty()) {	
-			if(tokenList.contains(jsonObject.optString("token"))){
+		if (!jsonObject.optString("token").isEmpty() || !jsonObject.optString("pseudonym").isEmpty()) {
+			// Token muss mit pseudonym übereinstimmen. -> HashMap
+			if(tokenList.get(jsonObject.optString("pseudonym")).equals(jsonObject.optString("token"))){
 				JSONObject successJsonObject = new JSONObject();
+				//SimpleDateFormat formatierung statt Date();
 				successJsonObject.put("success", "true").put("expire-Date", new Date());
 				return Response.ok().entity(successJsonObject.toString()).build();
 			}else{
@@ -76,10 +97,10 @@ public class Login {
 	}
 
 
-	private String createToken(String stringToEncode) {
+	private String createToken(String pseudonym,String stringToEncode) {
 		byte[] encoded = Base64.encodeBase64(stringToEncode.getBytes());
 		String token = new String(encoded);
-		tokenList.add(token);
+		tokenList.put(pseudonym, token);
 		return token;
 	}
 
@@ -93,11 +114,16 @@ public class Login {
 			return false;
 		}
 	}
-
+	
+	
+	/**
+	 * Dient nur darum ein name und password hinzuzufügen
+	 * @return
+	 */
 	@PUT
 	@Path("/setuppassword")
 	public Response setupPassword() {
-		passwordList.put("dennis", createToken("test"));
+		passwordList.put("dennis", createToken("dennis","test"));
 		return Response.ok().build();
 
 	}
