2f76603a9072b3e2ed01ad45aa9d045822e358ca
1521092Dennis <1521092@stud.hs-mannheim.de>
2017-06-12 15:08:49 +0200

    aaaaa

---
+++ ChatServer/simpe_JAX_server/src/server/Message.java
@@ -0,0 +1,77 @@
+package server;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.codehaus.*;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+
+
+public class Message {
+	String from;
+	String to;
+	Date date;
+	String text;
+	int sequenceNr;
+	
+
+	
+	public Message(String from, String to, Date date, String text, int sequenceNr){
+		this.from = from;
+		this.to = to;
+		this.date = date;
+		this.text = text;
+		this.sequenceNr = sequenceNr;
+	}
+	
+	public Message(String from, String to, Date date, String text){
+		this(from, to, date, text, 0);
+	}
+	
+	
+	public static Message transferJSONinMessage(String jsonObject) throws ParseException, JSONException{
+		Date date=null;
+		try {
+			JSONObject obj = new JSONObject(jsonObject);
+			if (obj.has("date")) {
+				String datum = obj.getString("date");
+				SimpleDateFormat sdf = new SimpleDateFormat(RESTfulWebServices.ISO8601);
+				date = sdf.parse(datum);
+			}
+
+			return new Message(obj.optString("from"), obj.optString("to"), date, obj.optString("text"),
+					obj.optInt("sequence"));
+		} catch (JSONException ex) {
+			throw new ParseException("String was not a valid JSON Message object.", -1);
+		}
+		
+			
+	}
+	
+	public JSONObject transferInJSONObject() throws JSONException{
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("from", this.from)
+					.put("to", this.to)
+					.put("date", new SimpleDateFormat(RESTfulWebServices.ISO8601).format(date))
+					.put("text", this.text)
+					.put("sequence", sequenceNr);
+		
+		return jsonObject;
+	}
+	
+	/**
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+        SimpleDateFormat sdf = new SimpleDateFormat(RESTfulWebServices.ISO8601);
+
+        return String.format("{ 'from': '%s', 'to': '%s', 'date': '%s', 'text': '%s'}".replace('\'',  '"'),
+                from, to, sdf.format(new Date()), text);
+    }
+	
+	
+
+}
---
+++ ChatServer/simpe_JAX_server/src/server/UserID.java
@@ -0,0 +1,71 @@
+package server;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class UserID {
+	/**
+	 * Name des Users
+	 */
+	String name;
+	/**
+	 * Sequenznummer
+	 */
+	int sequenceNr = 0;
+	
+	/**
+	 * <code>List</code> zum Speichern von Nachrichten
+	 */
+	public List<Message> messageList = new ArrayList<Message>();
+	
+	/**
+	 * Konstruktor der UserID
+	 * @param name Übergebener Name als String
+	 */
+	public UserID(String name){
+		this.name = name;
+	}
+	
+	/**
+	 * Erhöht die Sequenznummer einer Nachricht und fügt diese einer <code>messageList</code> hinzu. 
+	 * @param message Übergebene Nachricht
+	 * @return	Nachricht
+	 */
+	public Message sendMessage(Message message){
+		message.sequenceNr = sequenceNr++;
+		messageList.add(message);
+		return message;
+	}
+	
+	/**
+	 * Fügt einer ArrayList recievableMessages alle Messages hinzu, bei dennen die Sequenznummer entweder gleich 0 oder 
+	 * grösser ist als die Sequenznummer in den Messages die in <code>messageList</code> enthalten sind.
+	 * 
+	 * Sind alle Nachrichten in der <code>recievableMessages</code> enthalten, wird jede Nachricht aus der
+	 * <code>messageList</code> entfernt solange die <code>messageList</code> nicht leer ist oder die Sequenznummer
+	 * der Nachricht kleiner oder gleich der Übergeben Sequenznummer ist. 
+	 * @param sequenceNr	Übergebene Sequenznummer
+	 * @return <code>List</code> mit Nachrichten.
+	 */
+	public List<Message> revieveMessage(int sequenceNr){
+		List<Message> recievableMessages = new ArrayList<>();
+		for(Message message: messageList){
+			if(sequenceNr == 0 || message.sequenceNr >= sequenceNr){
+				recievableMessages.add(message);
+				
+			}
+		}
+		while(!messageList.isEmpty()){
+			int i=1;
+			Message message = this.messageList.get(messageList.size()-i);
+			if(message.sequenceNr < sequenceNr){
+				messageList.remove(messageList.size()-i);
+			}else{
+				i++;
+			}
+		}
+		return recievableMessages;	
+	}	
+	
+}
