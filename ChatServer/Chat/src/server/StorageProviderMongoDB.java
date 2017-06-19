75d7b614af311fb0f1fe3283c24facdaf5e7219a
kurnizki77 <kurnizkij@gmail.com>
2017-06-19 14:21:07 +0200

    login&server

--- ChatServer/Chat/src/server/StorageProviderMongoDB.java
+++ ChatServer/Chat/src/server/StorageProviderMongoDB.java
@@ -1,6 +1,16 @@
 package server;
 
 import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Filters.gte;
+import static com.mongodb.client.model.Filters.lte;
+import static com.mongodb.client.model.Filters.and;
+
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 import org.bson.Document;
 
 import com.mongodb.MongoClient;
@@ -48,6 +58,52 @@ class StorageProviderMongoDB {
 				 .append("sequence", message.sequenceNr).append("text", message.text);
 
 		collectionMessages.insertOne(document);
+	}
+
+	public synchronized List<Message> retrieveMessages(String userID, int sequence, boolean delete)
+			throws ParseException {
+		MongoCollection<Document> collectionMessages = database.getCollection("messages");
+
+		List<Document> list = (List<Document>) collectionMessages.find(and(eq("to", userID), gte("sequence", sequence)))
+				.into(new ArrayList<Document>());
+
+		if (list == null) {
+			return null;
+		}
+		if (delete == true) {
+			collectionMessages.deleteMany((and(eq("to", userID))));
+		}
+
+		List<Message> messageList = new ArrayList<Message>();
+
+		for (Document d : list) {
+			messageList.add(new Message(d.getString("token"), d.getString("from"), d.getString("to"),
+					d.getDate("date"), d.getString("text"),
+					d.getInteger("sequence")));
+		}
+		Collections.sort(messageList, new Comparator<Message>() {
+			@Override
+			public int compare(Message o1, Message o2) {
+				return o1.sequenceNr - o2.sequenceNr;
+			}
+		});
+		return messageList;
+	}
+
+	public synchronized int getUpdatedSequence(String user_id){
+		MongoCollection<Document> collectionSequence = database.getCollection("sequence");
+		Document doc=collectionSequence.find(eq("pseudonym",user_id)).first();
+		if(doc!=null){
+			doc.replace("sequence", doc.getInteger("sequence")+1);
+			collectionSequence.deleteOne(eq("pseudonym",user_id));
+			collectionSequence.insertOne(doc);
+			return doc.getInteger("sequence");
+		}else{
+			Document doc1=new Document("sequence",0).append("pseudonym", user_id);
+
+			collectionSequence.insertOne(doc1);
+			return 0;
+		}
 	}
 
 
--- ChatServer/Chat/src/server/UserID.java
+++ ChatServer/Chat/src/server/UserID.java
@@ -1,8 +1,16 @@
 package server;
 
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.ws.rs.core.MediaType;
+
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
 
 public class UserID {
 	/**
@@ -13,59 +21,102 @@ public class UserID {
 	 * Sequenznummer
 	 */
 	int sequenceNr = 0;
+	Date date;
+
+	private String token;
+	String hashedPassword;
 
 	/**
 	 * <code>List</code> zum Speichern von Nachrichten
 	 */
-	public List<Message> messageList = new ArrayList<Message>();
+	 public List<Message> messageList = new ArrayList<Message>();
 
 	/**
 	 * Konstruktor der UserID
-	 * @param name Übergebener Name als String
+	 *
+	 * @param name
+	 *            Übergebener Name als String
 	 */
-	public UserID(String name){
+	public UserID(String name) {
 		this.name = name;
+		this.sequenceNr=0;
 	}
 
 	/**
-	 * Erhöht die Sequenznummer einer Nachricht und fügt diese einer <code>messageList</code> hinzu.
-	 * @param message Übergebene Nachricht
-	 * @return	Nachricht
+	 * Erhöht die Sequenznummer einer Nachricht und fügt diese einer
+	 * <code>messageList</code> hinzu.
+	 *
+	 * @param message
+	 *            Übergebene Nachricht
+	 * @return Nachricht
 	 */
-	public Message sendMessage(Message message){
+	public Message sendMessage(Message message) {
 		message.sequenceNr = sequenceNr++;
 		messageList.add(message);
 		return message;
 	}
 
 	/**
-	 * Fügt einer ArrayList recievableMessages alle Messages hinzu, bei dennen die Sequenznummer entweder gleich 0 oder
-	 * grösser ist als die Sequenznummer in den Messages die in <code>messageList</code> enthalten sind.
+	 * Fügt einer ArrayList recievableMessages alle Messages hinzu, bei dennen
+	 * die Sequenznummer entweder gleich 0 oder grösser ist als die
+	 * Sequenznummer in den Messages die in <code>messageList</code> enthalten
+	 * sind.
 	 *
-	 * Sind alle Nachrichten in der <code>recievableMessages</code> enthalten, wird jede Nachricht aus der
-	 * <code>messageList</code> entfernt solange die <code>messageList</code> nicht leer ist oder die Sequenznummer
-	 * der Nachricht kleiner oder gleich der Übergeben Sequenznummer ist.
-	 * @param sequenceNr	Übergebene Sequenznummer
+	 * Sind alle Nachrichten in der <code>recievableMessages</code> enthalten,
+	 * wird jede Nachricht aus der <code>messageList</code> entfernt solange die
+	 * <code>messageList</code> nicht leer ist oder die Sequenznummer der
+	 * Nachricht kleiner oder gleich der Übergeben Sequenznummer ist.
+	 *
+	 * @param sequenceNr
+	 *            Übergebene Sequenznummer
 	 * @return <code>List</code> mit Nachrichten.
 	 */
-	public List<Message> revieveMessage(int sequenceNr){
+	public List<Message> revieveMessage(int sequenceNr) {
 		List<Message> recievableMessages = new ArrayList<>();
-		for(Message message: messageList){
-			if(sequenceNr == 0 || message.sequenceNr >= sequenceNr){
+		for (Message message : messageList) {
+			if (sequenceNr == 0 || message.sequenceNr >= sequenceNr) {
 				recievableMessages.add(message);
 
 			}
 		}
-		while(!messageList.isEmpty()){
-			int i=1;
-			Message message = this.messageList.get(messageList.size()-i);
-			if(message.sequenceNr < sequenceNr){
-				messageList.remove(messageList.size()-i);
-			}else{
+		while (!messageList.isEmpty()) {
+			int i = 1;
+			Message message = this.messageList.get(messageList.size() - i);
+			if (message.sequenceNr < sequenceNr) {
+				messageList.remove(messageList.size() - i);
+			} else {
 				i++;
 			}
 		}
 		return recievableMessages;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+
+	protected static final String uri = "http://141.19.142.60:5001";
+
+	public boolean auth() {
+		Client client = Client.create();
+		JSONObject object = new JSONObject();
+		try {
+			object.put("token", this.token);
+
+			object.put("pseudonym", this.name);
+		} catch (JSONException e) {
+			e.printStackTrace();
+			return false;
+		}
+		ClientResponse antwort = client.resource(uri + "/auth").accept(MediaType.APPLICATION_JSON)
+				.type(MediaType.APPLICATION_JSON).post(ClientResponse.class, object.toString());
+		client.destroy();
+
+		if (antwort.getStatus() != 200) {
+			System.out.println(antwort.getStatus());
+			return false;
+		}
+		return true;
 	}
 
 }
